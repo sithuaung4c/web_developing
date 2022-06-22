f9d50cae95aebbedf960edf55134a0c97b979a14
sithuaung4c <106944626+sithuaung4c@users.noreply.github.com>
2022-06-22 23:07:42 +0630

    Update testingweb.py

--- testingweb.py
+++ testingweb.py
@@ -5,8 +5,6 @@ import streamlit.components.v1 as components
 # >>> fig = px.box(range(10))
 # >>> fig.write_html('test.html')
 
-st.header("test html import")
-
 HtmlFile = open("bootstraptest.html", 'r', encoding='utf-8')
 source_code = HtmlFile.read() 
 components.html(source_code)
